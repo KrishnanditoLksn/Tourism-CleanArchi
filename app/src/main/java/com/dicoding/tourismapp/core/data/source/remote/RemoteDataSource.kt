cb921ed9dc17f0780cd1c32d3bac70c581408410
Gorthdzx <krislaksana123@gmail.com>
2024-12-31 18:23:32 +0700

    refactor: changed from json dummy data to api from network

--- app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
@@ -1,45 +1,47 @@
 package com.dicoding.tourismapp.core.data.source.remote
 
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.dicoding.tourismapp.core.data.source.remote.network.ApiResponse
+import com.dicoding.tourismapp.core.data.source.remote.network.ApiService
+import com.dicoding.tourismapp.core.data.source.remote.response.ListTourismResponse
 import com.dicoding.tourismapp.core.data.source.remote.response.TourismResponse
-import com.dicoding.tourismapp.core.utils.JsonHelper
-import org.json.JSONException
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class RemoteDataSource private constructor(private val jsonHelper: JsonHelper) {
+class RemoteDataSource private constructor(private val apiService: ApiService) {
     companion object {
         @Volatile
         private var instance: RemoteDataSource? = null
 
-        fun getInstance(helper: JsonHelper): RemoteDataSource =
+        fun getInstance(apiService: ApiService): RemoteDataSource =
             instance ?: synchronized(this) {
-                instance ?: RemoteDataSource(helper)
+                instance ?: RemoteDataSource(apiService)
             }
     }
 
     fun getAllTourism(): LiveData<ApiResponse<List<TourismResponse>>> {
         val resultData = MutableLiveData<ApiResponse<List<TourismResponse>>>()
 
-        //get data from local json
-        val handler = Handler(Looper.getMainLooper())
-        handler.postDelayed({
-            try {
-                val dataArray = jsonHelper.loadData()
-                if (dataArray.isNotEmpty()) {
-                    resultData.value = ApiResponse.Success(dataArray)
-                } else {
-                    resultData.value = ApiResponse.Empty
-                }
-            } catch (e: JSONException){
-                resultData.value = ApiResponse.Error(e.toString())
-                Log.e("RemoteDataSource", e.toString())
-            }
-        }, 2000)
+        val client = apiService.getList()
 
+        client.enqueue(object : Callback<ListTourismResponse> {
+            override fun onResponse(
+                call: Call<ListTourismResponse>,
+                response: Response<ListTourismResponse>
+            ) {
+                val datas = response.body()?.places
+                resultData.value =
+                    if (datas != null) ApiResponse.Success(datas) else ApiResponse.Empty
+            }
+
+            override fun onFailure(p0: Call<ListTourismResponse>, p1: Throwable) {
+                resultData.value = ApiResponse.Error(p1.message.toString())
+                Log.e("RemoteDataSource", p1.message.toString())
+            }
+        })
         return resultData
     }
 }
