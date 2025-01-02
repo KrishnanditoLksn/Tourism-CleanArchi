024885c5b57374485b3f4fc083ddb4753d100d16
Gorthdzx <krislaksana123@gmail.com>
2025-01-02 12:28:13 +0700

    feat: added return value as livedata

--- app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteViewModel.kt
@@ -1,11 +1,12 @@
 package com.dicoding.tourismapp.favorite
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.toLiveData
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 
 class FavoriteViewModel(tourismRepository: TourismUseCase) : ViewModel() {
 
-    val favoriteTourism = tourismRepository.getFavoriteTourism()
+    val favoriteTourism = tourismRepository.getFavoriteTourism().toLiveData()
 
 }
 
